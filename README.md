fea0b6850112fdf96dc02dfbce7a8ef2f4ada780
PolinaSwetlova <139793855+PolinaSwetlova@users.noreply.github.com>
2023-11-27 20:26:49 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# final_api_Yatube_swetlova
+# Описание
+
+Проект представляет собой API для проекта yatube.
+
+Для аутентификации использованы JWT-токены.
+
+У неаутентифицированных пользователей доступ к API только на чтение.
+
+Аутентифицированным пользователям разрешено изменение и удаление своего контента; в остальных случаях доступ предоставляется только для чтения.
+
+# Установка
+
+Клонировать репозиторий и перейти в него в командной строке:
+
+git clone git@github.com:PolinaSwetlova/api_final_yatube.git
+
+cd api_final_yatube
+
+Cоздать и активировать виртуальное окружение:
+
+python -m venv venv
+
+source venv/Scripts/activate
+
+Установить зависимости из файла requirements.txt:
+
+python -m pip install --upgrade pip
+
+pip install -r requirements.txt
+
+Выполнить миграции:
+
+python manage.py migrate
+
+Запустить проект:
+
+python manage.py runserver
+
+# Примеры
+
+Для доступа к API необходимо получить токен: 
+Нужно выполнить POST-запрос localhost:8000/api/v1/token/ передав поля username и password. API вернет JWT-токен
+
+Дальше, передав токен можно будет обращаться к методам, например: 
+
+/api/v1/posts/ (GET, POST, PUT, PATCH, DELETE)
+
+При отправке запроса передавайте токен в заголовке Authorization: Bearer <токен>
---
+++ yatube_api/api/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import get_object_or_404
+from django_filters.rest_framework import DjangoFilterBackend
+from posts.models import Group, Post, User, Comment
+from rest_framework import filters, permissions, status, viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
+
+from .permissions import IsAuthorOrReadOnly
+from .serializers import (
+    CommentSerializer,
+    FollowSerializer,
+    GroupSerializer,
+    PostSerializer,
+)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['group']
+    pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthorOrReadOnly, IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        return Comment.objects.filter(post=post)
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = [IsAuthorOrReadOnly]
+
+    def create(self, request):
+        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    http_method_names = ['get', 'post']
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = FollowSerializer
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['user__username', 'following__username']
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.request.user.username)
+        return user.follower
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
